7cd9c845cd2d4517950624d552e7e4e9340acc73
Volkarl <karlzone0@gmail.com>
2016-04-15 08:36:41 +0200

    HandleUserInput

---
+++ Stegomatic/Miscellaneous/HandleUserInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stegomatic.Miscellaneous
+{
+    class HandleUserInput
+    {
+        public void File(string path)
+        {
+
+        }
+
+        public void Message(string message)
+        {
+
+        }
+    }
+}

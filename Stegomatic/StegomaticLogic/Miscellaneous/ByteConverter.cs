4b53376d09160d494d82fd2ffbe2169706506a0d
Karlzone <Karlzone0@gmail.com>
2016-04-15 13:49:46 +0200

    Finpudset klassestruktur lidt

--- Stegomatic/StegomaticLogic/Miscellaneous/ByteConverter.cs
+++ Stegomatic/StegomaticLogic/Miscellaneous/ByteConverter.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Stegomatic.StegomaticLogic
+namespace StegomaticProject.StegomaticLogic
 {
     static public class ByteConverter
     {

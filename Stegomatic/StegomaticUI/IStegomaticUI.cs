7b57dcb7fba10846fea790e48c34c23cdf657847
Elyas Bassam Bahodi <ebahod15@student.aau.dk>
2016-04-27 14:01:59 +0200

    awfsdfa

--- Stegomatic/StegomaticUI/IStegomaticUI.cs
+++ Stegomatic/StegomaticUI/IStegomaticUI.cs
@@ -8,5 +8,12 @@ namespace StegomaticProject.StegomaticUI
 {
     public interface IStegomaticUI
     {
+        void OpenImage();
+        void EncodeImage();
+        void DecodeImage();
+        void SaveImage();
+        void ToggleCompression();
+        void ToggleCryptography();
+        void ToggleConfounder();
     }
 }

7dc8a2516067288db758d45d99423b7d7a1458b4
Volkarl <karlzone0@gmail.com>
2016-04-27 10:29:05 +0200

    Small change

--- Stegomatic/StegomaticController/IStegoControl.cs
+++ Stegomatic/StegomaticController/IStegoControl.cs
@@ -10,7 +10,7 @@
 
 
         void Run(string path, string message);
-        void ToggleEmbedOrExtract(ToggleEvent e);  // Skal kunne fjerne streng-indtastnings-feltet og lave et output felt
+        //void ToggleEmbedOrExtract(ToggleEvent e);  // Skal kunne fjerne streng-indtastnings-feltet og lave et output felt Tror den skal væk den her.
         void OpenOptionsMenu(OpenUIElementEvent e); // Tager imod et delegate, der kan instaitiere WinForms vinduer
         void ToggleOption(ToggleOptionEvent e); // ToggleOptionEvent indeholder et tal fra 0 til maxKnapperDerKanÆndres
         void ShowPicture();

a7526422d52afd6bef1c441caf9badd9d0bc5ae9
EstTown <f_hansen@live.dk>
2016-05-02 14:42:13 +0200

    fuck fuck

--- Stegomatic/StegoSystemUI/PopupForm.cs
+++ Stegomatic/StegoSystemUI/PopupForm.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WinFormsGUI
+namespace StegomaticProject.StegoSystemUI
 {
     public partial class EncyptionkeyPopup : Form
     {

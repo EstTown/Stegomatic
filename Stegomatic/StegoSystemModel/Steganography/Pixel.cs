530f4cb45419d9dd52241f9448a1384f51790454
Hangover1234 <joa.theis@gmail.com>
2016-05-02 14:20:04 +0200

    Changed pixel class

--- Stegomatic/StegoSystemModel/Steganography/Pixel.cs
+++ Stegomatic/StegoSystemModel/Steganography/Pixel.cs
@@ -8,5 +8,28 @@ namespace StegomaticProject.StegoSystemModel.Steganography
 {
     class Pixel
     {
+        public short R { get; set; }
+        public short G { get; set; }
+        public short B { get; set; }
+        public short EmbeddedValue;
+        public short PosX;
+        public short PosY; 
+
+        void GetPixelPos ()
+        {
+        }
+
+        void GetPixelValue()
+        {
+        }
+
+        void AssignEmbeddedValue()
+        {
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Position X = {0}   Position Y = {1}   R = {2}  G = {3}  B = {4}", this.PosX, this.PosY, this.R, this.G, this.B);
+        }
     }
 }

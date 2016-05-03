a2b5277c6268c21f36f55c734f20eb8807e77640
Ruben <Ruben@RUBEN-LAPTOP>
2016-05-03 13:38:49 +0200

    Another minor correction

--- Stegomatic/StegoSystemModel/Steganography/GraphtheoryBased.cs
+++ Stegomatic/StegoSystemModel/Steganography/GraphtheoryBased.cs
@@ -67,7 +67,7 @@ namespace StegomaticProject.StegoSystemModel.Steganography
             return seed;
         }
 
-        private void GetRandomPixelsAddToList2(Bitmap image, string passphrase, int pixelsNeeded)
+        private void GetRandomPixelsAddToList2(Bitmap image, int pixelsNeeded, string passphrase)
         {
             int key = ShortenAndParsePassphraseToInt32(passphrase);
             int numberOfPixels = image.Width*image.Height;

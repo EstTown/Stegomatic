41ba50188356bde9f2b5a74a1537fcc451ae2c29
Palle Thillemann <palle95@gmail.com>
2016-05-03 11:43:28 +0200

    Tilføjet metode til at konverterer et byte-array om til bitpar der bliver tildelt værdierne 0-3 og føjet til en liste.

--- Stegomatic/StegoSystemModel/Steganography/GraphtheoryBased.cs
+++ Stegomatic/StegoSystemModel/Steganography/GraphtheoryBased.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace StegomaticProject.StegoSystemModel.Steganography
 {
@@ -34,6 +35,49 @@ namespace StegomaticProject.StegoSystemModel.Steganography
         {
             byte weight = 0;
             return weight;
+        }
+
+        /*Method for getting the value of bitpairs into a list of ints from a byte-array*/
+        public List< IEnumerable<int>> ChopBytesToBitPairs(byte[] byteArray)
+        {
+            /*List fo int values*/
+            List< IEnumerable<int>> messageValues = new List<IEnumerable <int>>();
+
+            foreach (byte value in byteArray)
+            {
+                messageValues.Add(ConvertBitsToInt(value));
+            }
+
+            return messageValues;
+        }
+
+        /*Method for converting bitpairs to ints from a byte*/
+        public IEnumerable<int> ConvertBitsToInt(byte byteValue)
+        {
+            int value;
+            BitArray bitValues = new BitArray(new byte[] { byteValue });
+
+            for (int index = 7; index > -1; index -= 2)   
+            {
+                if(bitValues[index] == true && bitValues[index - 1] == true)
+                {
+                    value = 3;
+                }
+                else if(bitValues[index] == true && bitValues[index - 1] == false)
+                {
+                    value = 2;
+                }
+                else if(bitValues[index] == false && bitValues[index - 1] == true)
+                {
+                    value = 1;
+                }
+                else
+                {
+                    value = 0;
+                }
+
+                yield return value;
+            }
         }
     }
 }

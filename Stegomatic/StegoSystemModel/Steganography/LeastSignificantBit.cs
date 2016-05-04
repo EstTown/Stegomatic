8e7b445bf2250cbc415a29436f867c8c39377048
Volkarl <karlzone0@gmail.com>
2016-05-04 15:59:04 +0200

    Start og implementing an LSB algorithm and getting the program to work. Correction to my previous change's comment: StegoSystemController is all but tested, not the StegoSystemModelClass.

---
+++ Stegomatic/StegoSystemModel/Steganography/LeastSignificantBit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StegomaticProject.StegoSystemModel.Steganography
+{
+    public class LeastSignificantBit : IStegoAlgorithm
+    {
+        public Bitmap Encode(Bitmap coverImage, string seed, byte[] message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Decode(Bitmap coverImage, string seed)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

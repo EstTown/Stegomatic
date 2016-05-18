d94e266dde56d3a349c0279d70f8b25e3c7f7720
EstTown <f_hansen@live.dk>
2016-05-18 22:04:24 +0200

    Added logo/icon to all windows (source: https://www.iconfinder.com/icons/728933/document_edit_file_page_paper_sheet_text_icon#size=128)

--- Stegomatic/StegoSystemUI/Form1.Designer.cs
+++ Stegomatic/StegoSystemUI/Form1.Designer.cs
@@ -322,6 +322,7 @@
             this.Controls.Add(this.btn_open);
             this.Controls.Add(this.picbox_image);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Steg-o-matic 1000";
